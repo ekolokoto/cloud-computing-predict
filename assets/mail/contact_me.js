8659d9cf15e52cd2a6c49658e86dd664c51ab355
ekolokoto <ekolokoto@sandtech.com>
2024-09-14 06:55:17 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://g7qjva6xm8.execute-api.eu-west-1.amazonaws.com",
+                url: "https://g7qjva6xm8.execute-api.eu-west-1.amazonaws.com/v1",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
